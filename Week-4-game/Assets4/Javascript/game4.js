79b50710ca612287056b6f4fa62ff094dd6df859
Morgan McGee <morgan@fanexperiences.com>
2016-04-29 17:15:19 -0400

    fixed js some  still not working

--- Week-4-game/Assets4/Javascript/game4.js
+++ Week-4-game/Assets4/Javascript/game4.js
@@ -1,63 +1,56 @@
- var crystalGame ={
- 	var wins = 0;
- 	var losses = 0;
- 	var scoreCount = 0;
 
-
+//}
  //where random number is displayed at the start of 
  // each game between 19-120
- 	
- 		var randomWinNumb = Math.floor(Math.random()*69) + 1
-$("#numberBox").html(randomWinNumb);
+ window.onload = function (){	
+ 	var total=0;
 
-//player wins game if they match their crystal from
-// to the number box- You won!! will be displayed or
-// You lost. will be displayed as well as the total 
-//being added to that section. game restarts after 
-//player won or lost
- 	$("#scoreBox").html("Wins: "  + wins++ + "Losses: " + losses++)
 
-//random number between 1-12, changes at the start of
-// each game
- 	$("#jewel1").on('click', function ()
- 			{
- 				var randomNumber = Math.floor(Math.random()*12) + 1
- 			console.log(randomNumber);
- 			scoreCount++;
- 			});
+function ranNum(min,max){
+	var randomNum = 0;
+	randomNum = Math.floor((Math.random () * (max - min + 1 )) + min);
+		return randomNum;  
+}
+	var randomNum = ranNum (19,120);
+	function  showNum (ranNum){
+		var result; 
+		document.getElementById("numberBox").innerHTML = ranNum;
 
-//random number between 1-12, changes at the start of
-// each game
- 	$("#jewel2").on('click', function()
- 			{
- 			var randomNumber = Math.floor(Math.random()*12) + 1
- 			console.log(randomNumber);
- 			scoreCount++;
- 			})
+	}
 
-//random number between 1-12, changes at the start of
-// each game
- 	$("#jewel3").on('click', function()
- 			{
- 			var randomNumber = Math.floor(Math.random()*12) + 1
- 			console.log(randomNumber);
- 			scoreCount++;
- 			})
 
-//random number between 1-12, changes at the start of
-// each game
- 	$("#jewel4").on('click', function()
- 			{
- 			var randomNumber = Math.floor(Math.random()*12) + 1
- 			console.log(randomNumber);
- 			scoreCount++;
- 			})
+function addCrystal(crystal){
+    total = total + crystal;
+    return total;
+  }
+  function showSum(crys){
+    var sum = addCrystal(crys);
+    document.getElementById("totalScoreBox").innerHTML = sum;
+    return sum;
+  }
+  function test_winner(sum){
+    if(sum == randomNumber){
+      document.getElementById("scoreBox").innerHTML = "You Win!"
+    }else if(sum =! randomNumber){
+      ocument.getElementById("scoreBox").innerHTML = "You Lose!"
+    }
 
-//when player clicks on crystal it will add to number
-// in this box
- 	$("#totalScoreBox").html(scorecount++)
- 	$("scoreBox").html(scoreCount)
+  }
 
- };
-
- crystalGame.setupGame();
+  showNum(randomNumber);
+  var crystal1 = ranNum(1, 12);
+  var crystal2 = ranNum(1, 12);
+  var crystal3 = ranNum(1, 12);
+  var crystal4 = ranNum(1, 12);
+  $('#jewel1').on('click', function(){
+    test_winner(showSum(crystal1));
+  });
+  $('#jewel2').on('click', function(){
+    test_winner(showSum(crystal2));
+  });
+  $('#jewel3').on('click', function(){
+    test_winner(showSum(crystal3));
+  });
+  $('#jewel4').on('click', function(){
+    test_winner(showSum(crystal4));
+  });
